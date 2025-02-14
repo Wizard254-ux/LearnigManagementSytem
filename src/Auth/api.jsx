745234930cb62f2f613c0593693d15f7bd599abc
Wizard254-ux <brianndesa262@gmail.com>
2025-02-14 10:15:23 +0300

    ..

--- src/Auth/api.jsx
+++ src/Auth/api.jsx
@@ -3,8 +3,8 @@ import { useAuth } from './AuthProvider';
 import { getGlobalLogout } from './AuthProvider';
 // Create axios instance
 export const api = axios.create({
-  // baseURL: 'https://lmsbackendexpress.onrender.com/',
-  baseURL: 'http://localhost:5000/',
+  baseURL: 'https://lmsbackendexpress.onrender.com/',
+  // baseURL: 'http://localhost:5000/',
   withCredentials: true, // Important for sending cookies
   headers: {
     'Content-Type': 'application/json',
